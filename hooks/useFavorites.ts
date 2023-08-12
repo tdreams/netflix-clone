4a6cdf6ca8eaf2fa14a1b081cc6333cf17191372
tdreams <fabriels@gmail.com>
2023-08-12 14:53:57 -0700

    try

--- hooks/useFavorites.ts
+++ hooks/useFavorites.ts
@@ -1,8 +1,16 @@
 import axios from "axios";
 import { Movie } from "@/types";
+import { currentUser } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 const UseFavorites = async () => {
   try {
+    const user = await currentUser();
+    if (!user) {
+      return new NextResponse("Missing userId in query parameters", {
+        status: 400,
+      });
+    }
     const response = await axios.get("http://localhost:3000/api/favorites");
     console.log("list Favorites", response.data);
     if (Array.isArray(response.data) && response.data.length > 0) {
