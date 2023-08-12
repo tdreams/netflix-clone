ba7ebed349de476b420a1832008c961e9f3326e2
tdreams <fabriels@gmail.com>
2023-08-11 22:53:23 -0700

    routes

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,12 @@ import { authMiddleware } from "@clerk/nextjs";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/nextjs/middleware for more information about configuring your middleware
 export default authMiddleware({
-  publicRoutes: ["/api/webhooks/user"],
+  publicRoutes: [
+    "/api/webhooks/user",
+    "/api/favorites",
+    "/api/movies",
+    "/api/random",
+  ],
 });
 
 export const config = {
