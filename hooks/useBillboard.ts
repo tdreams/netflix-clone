fdedf9be3263b882311fc1c54736229fad0173cc
tdreams <fabriels@gmail.com>
2023-08-12 17:48:06 -0700

    try

--- hooks/useBillboard.ts
+++ hooks/useBillboard.ts
@@ -2,7 +2,7 @@ import { Movie } from "@/types";
 
 const FetchRandomMovie = async () => {
   try {
-    const response = await fetch("http://localhost:3000/api/random");
+    const response = await fetch("/api/random");
     const data = await response.json();
 
     if (data.length > 0) {
